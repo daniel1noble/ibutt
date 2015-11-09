e0053191c4ad7fcd37cea718e3231e0c2a99dc58
Daniel Noble <daniel.wa.noble@gmail.com>
2015-11-10 08:24:57 +1100

    Added more documentation

--- R/import.func.R
+++ R/import.func.R
@@ -1,5 +1,5 @@
-
-# Import a list of .csv files generated from iButtons and Hydrochrons. Save these files to a list and name the data.frames the file names
+#' Import a list of .csv files generated from iButtons and Hydrochrons. Save these files to a list and name the data.frames the file names
+#' @param dir Directory path to the folder containing the temperature/humidity data. Files should be .csv.
 
 import <- function(dir){
 	list <- as.list(list.files(dir, pattern = ".csv"))
--- R/process.R
+++ R/process.R
@@ -1,11 +1,12 @@
+#' Function for processing the text read from ibuttons. Still needs work.
+#' @param dat a text/csv string imported from a directory containing csv files for ibutton data.
 
-# Function for processing the text read from ibuttons. Still needs lots of work. 
 process <- function(dat){
-	tmp <- grep("[0-9]+/[0-9]+/[0-9]", dat, value = TRUE) ## Extract relevant lines
-	date <- as.Date(gsub("\\s[0-9]+:.*", "",  tmp),"%d/%m/%y") ## Will get the date
-	AMPM <- gsub(".*\\s([A-Z]+).*", "\\1",  tmp) ## Will AM/PM
-	time <- gsub(".*\\s([0-9]+:[0-9]+:[0-9]+).*", "\\1", tmp) ## Will get the time
-	unit <- gsub(".*,?([RHC]),.*", "\\1", tmp)
+	     tmp <- grep("^[0-9]+/", dat, value = TRUE) ## Extract relevant lines
+	    date <- as.Date(gsub("\\s[0-9]+:.*", "",  tmp),"%d/%m/%y") ## Will get the date
+	    AMPM <- gsub(".*\\s([A-Z]+).*", "\\1",  tmp) ## Will AM/PM
+	    time <- gsub(".*\\s([0-9]+:[0-9]+:[0-9]+).*", "\\1", tmp) ## Will get the time
+	    unit <- gsub(".*,?([RHC]),.*", "\\1", tmp)
 	temp.hum <- as.numeric(gsub(".*,([0-9]+.*)", "\\1", tmp))
 	#date.time <- as.Date(strftime(gsub("(^[0-9].*\\s[0-9].*\\s[APM]+),.*", "\\1",  tmp),"%d/%m/%y %I:%M:%S %p"), "%d/%m/%y %I:%M:%S %p") ## Will get the date and time and format correct
 
