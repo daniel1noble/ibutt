52dc122465dab5709968d4ed26094728dc675612
Daniel Noble <daniel.wa.noble@gmail.com>
2015-11-10 22:50:28 +1100

    Added some new date/time formatting which should make things easy to plot and to query

--- R/process.R
+++ R/process.R
@@ -2,14 +2,17 @@
 #' @param dat a text/csv string imported from a directory containing csv files for ibutton data.
 
 process <- function(dat){
-	     tmp <- grep("^[0-9]+/", dat, value = TRUE) ## Extract relevant lines
+	tmp <- grep("^[0-9]+/", dat, value = TRUE) ## Extract relevant lines
+	    
+	    #Do all the processing of the text.
 	    date <- as.Date(gsub("\\s[0-9]+:.*", "",  tmp),"%d/%m/%y") ## Will get the date
 	    AMPM <- gsub(".*\\s([A-Z]+).*", "\\1",  tmp) ## Will AM/PM
 	    time <- gsub(".*\\s([0-9]+:[0-9]+:[0-9]+).*", "\\1", tmp) ## Will get the time
+	  time24 <- substr(strptime(time, format "%I:%M:%S %p"))
 	    unit <- gsub(".*,?([RHC]),.*", "\\1", tmp)
 	temp.hum <- as.numeric(gsub(".*,([0-9]+.*)", "\\1", tmp))
-	#date.time <- as.Date(strftime(gsub("(^[0-9].*\\s[0-9].*\\s[APM]+),.*", "\\1",  tmp),"%d/%m/%y %I:%M:%S %p"), "%d/%m/%y %I:%M:%S %p") ## Will get the date and time and format correct
+	date.time <- chron::chron(dates = date, times = time24, format = c('y-m-d', 'h:m:s'))
 
-	return(data.frame(Date = date, Time = paste0(time ,AMPM), Units = ifelse(unit == "C", unit, paste0("R",unit)), Temperature.Humidity = temp.hum))
+	return(data.frame(Date.time = date.time, Date = date, Time = paste0(time ,AMPM), Units = ifelse(unit == "C", unit, paste0("R",unit)), Temperature.Humidity = temp.hum))
 }
 
