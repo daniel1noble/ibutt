130b529355418b6ecfd22d0f9ab8f96db1a7d3b7
Daniel Noble <daniel.wa.noble@gmail.com>
2016-03-03 21:30:26 +1100

    Updated tests

--- tests/testthat/test-ibuttProc.R
+++ tests/testthat/test-ibuttProc.R
@@ -1,8 +1,8 @@
 context("Check correct data processing")
 
-test_that("Check that processDat is working right", {
+test_that("Check that processDat is working right by testing data class", {
 	dir <- paste0(system.file("extdata",  package="ibutt"),"/")
-	x <-  ibuttProc(dir, date = "2015-11-06", time = c("10:00", "11:00"), list = TRUE)
+	x   <-  ibuttProc(dir, date = "2015-11-06", time = c("10:00", "11:00"), list = TRUE)
 	x2 <-  ibuttProc(dir, date = "2015-11-06", time = c("10:00", "11:00"), list = FALSE)
 
 	expect_is(x, "list", info = "x is not a list as expected")
